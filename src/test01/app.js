a5a1b2190711a95fc5c857ed975d9dc6add34157
TDBFG\TAMANS3 <anselm.tam@td.com>
2017-04-21 16:12:38 -0400

    Validated and massaged json data Created a http service call inside my app controller using the GET method to grab the json data and create an array of butons using the ng-repeat directive Created a View in app.html using bootstrap classes Styled the view using sass including variables, imports, and extends Tried to create a smaccs based architecture using partials and modules

--- src/test01/app.js
+++ src/test01/app.js
@@ -1,8 +1,24 @@
 export const test01 = {
-  template: require('./app.html'),
-  controller() {
-    
-    // your code here
+	template: require('./app.html'),
+	controller($http) {
+		var state = this;
+		state.data = [];
 
-  }
+
+//returns a promise based callback of the json data 
+//Running a nested for loop to populate the array and then push out a new array to format the data 
+
+		$http.get('test01/data.json').then(function(result) {
+			for (var prop in result.data) {
+				var item = result.data[prop];
+				for (var i in item) {
+					state.data.push(item[i])
+				}
+			}
+		});
+
+		this.state = state;
+	}
+
 };
+
